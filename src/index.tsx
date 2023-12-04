9a8a2b01bcf63941b4dafeee39ecd06743118221
chenyuhao <cccchenyuhao@gmail.com>
2023-12-04 17:19:07 +0800

    vite init

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import "./style.css";
-import App from "./App";
-
-const element = document.getElementById("root") as HTMLDivElement;
-const root = createRoot(element);
-root.render(<App />);
