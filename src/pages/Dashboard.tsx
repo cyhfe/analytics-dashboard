550a21703b5703fabd6b8c2b61d382e791c0da42
chenyuhao <cccchenyuhao@gmail.com>
2023-12-04 18:16:23 +0800

    stash

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,27 @@
+const data = [
+  { title: "访问人数", value: "xx人" },
+  { title: "访问次数", value: "xx人" },
+  { title: "全页面浏览量", value: "xx人" },
+  { title: "人均页面浏览", value: "xx人" },
+  { title: "平均浏览时间", value: "xx人" },
+];
 function Dashboard() {
   return (
-    <div>
-      <div>header</div>
+    <div className="container">
+      <div className="flex">
+        <div>select website</div>
+        <div>当前在线 xx 人</div>
+      </div>
+      <div className="flex">
+        {data.map((d) => {
+          return (
+            <div>
+              <div>{d.title}</div>
+              <div>{d.value}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
